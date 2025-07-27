2c59c3f7e90e43aafa948b58bc35a1ddfdf98a5f
RSaminathrao <rsaminathrao777@gmail.com>
2025-07-27 06:04:22 -0700

    Time: 2 ms (20.73%), Space: 7.7 MB (82.15%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.c
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.c
@@ -1,15 +1,20 @@
 bool judgeSquareSum(int c) {
-    int r=sqrt(c);
-    long s=0,j;
-    while(s<=r)
-    {
-        j=s*s+r*r;
-        if(j==c)
-            return true;
-        else if(j<c)
-            s++;
-        else
-            r--;
+//   for(long i=0;i<=c;i++)
+//   {
+//     for(long j=i;j<=c;j++)
+//     {
+//         if(i*i+j*j==c)
+//             return true;
+//     }
+//   }
+//   return false;  
+   
+    long long i = 0, j = floor(sqrt(c));
+    while (i <= j) {
+        long long sum = i*i + j*j;
+        if (sum == c) return true;
+        else if (sum < c) i++;
+        else j--;
     }
     return false;
 }
