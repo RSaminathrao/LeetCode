f09a1478cd339a91c2e3fa28b96055afd4f6680c
RSaminathrao <rsaminathrao777@gmail.com>
2025-08-15 22:06:55 -0700

    Time: 3 ms (13.5%), Space: 40.7 MB (44.6%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximum69Number (int num) {
-       String n=Integer.toString(num);
+       String n=String.valueOf(num);
        return Integer.valueOf(n.replaceFirst("6","9"));
        
     }
